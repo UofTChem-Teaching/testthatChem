59fca19a393e29451dbe03fea652d4a1cc60cec3
Mona Lisa Kalulu <23731053+kevinfinnlee@users.noreply.github.com>
2022-08-08 00:03:47 -0400

    repackaged

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,10 @@
-# Hello, world!
+# ChemTest routine and autoGrading for 210 and 310
 #
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
+#Import distributed packages from Github directly
+#
+#install.packages("devtools")
+#devtools::install_github("UofTChem-Teaching/testthat_chem")
+#library(testthat_chem)
 #
 # You can learn more about package authoring with RStudio at:
 #
@@ -13,6 +16,112 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+#' @export
+
+test_column_names <- function(data, expected, name = "the data") {
+  description <-
+    paste("check if", name, "contains the correct column names")
+  test_that(description, {
+    expect_named(data, expected)
+  })
 }
+
+test_dimensions <- function(data, expected, name = "the data") {
+  description <-
+    paste("check if", name, "has the correct number of rows and columns")
+  test_that(description, {
+    expect_equal(dim(data), !!expected)
+  })
+}
+
+test_column_type <-
+  function(data, col_name, expected, name = "the data") {
+    description <-
+      paste("check if",
+            name,
+            "has a column named",
+            col_name,
+            "with type",
+            expected)
+    test_that(description, {
+      expect_type(data[[col_name]], expected)
+    })
+  }
+
+test_entry <-
+  function(data, col_name, row_index, expected, name = "the data") {
+    description <-
+      paste("check if",
+            name,
+            "has the correct value at row",
+            row_index,
+            "and column",
+            col_name)
+    test_that(description, {
+      expect_equal(data[[col_name]][row_index],!!expected)
+    })
+  }
+
+test_NA <- function(data, col_name, expected, name = "the data") {
+  description <-
+    paste("check if cells inside", name, "contains NA invalid rows")
+  test_that(description, {
+    expect_equal(any(is.na(data[[col_name]])),!!expected)
+  })
+}
+
+test_dimensions_plot <-
+  function(data, col_name, expected, name = "the data") {
+    description <-
+      paste("check if",
+            name,
+            "- column",
+            col_name,
+            "has the correct number of rows and columns")
+    test_that(description, {
+      expect_equal(dim(data[[col_name]]), !!expected)
+    })
+  }
+
+test_column_type_plot <-
+  function(data,
+           table_name,
+           col_name,
+           expected,
+           name = "the data") {
+    description <-
+      paste("check if",
+            name,
+            "has a column named",
+            col_name,
+            "with type",
+            expected)
+    test_that(description, {
+      expect_type(data[[table_name]][[col_name]], expected)
+    })
+  }
+
+test_entry_plot <-
+  function(data,
+           col_name,
+           row_index,
+           lb,
+           expected,
+           name = "the data") {
+    description <-
+      paste(
+        "check if",
+        name,
+        "has the correct value of",
+        expected,
+        "at row",
+        row_index,
+        "of the column:",
+        col_name,
+        ",level:",
+        lb
+      )
+    test_that(description, {
+      expect_equal(data[[col_name]][row_index][[lb]], !!expected)
+    })
+  }
